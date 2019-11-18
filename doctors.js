f3924f24f338ea99b4549920987ca01b74c9a1d6
Ryan Lobro <56172426+lobrovir@users.noreply.github.com>
2019-11-18 15:53:16 -0800

    Update doctors.js

--- doctors.js
+++ doctors.js
@@ -1,51 +1,46 @@
-module.exports = function(){
-    var express = require('express');
-    var router = express.Router();
+module.exports = function() {
+  var express = require('express');
+  var router = express.Router();
 
-    function getDoctors(res, mysql, context, complete){
-        mysql.pool.query("SELECT doctor.ID as id, first_name, last_name, C_ID FROM doctor, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.people = results;
-            complete();
-        });
+  function getDoctors(res, pharmacy, context, complete) {
+    pharmacy.pool.query("SELECT ID, first_name, last_name, C_ID FROM doctor, function(error, results, fields){
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.doctors = results; complete();
+    });
+}
+
+router.get('/', function(req, res) {
+  var callbackCount = 0;
+  var context = {};
+  var pharmacy = req.app.get('pharmacy');
+  getDoctors(res, pharmacy, context, complete);
+
+  function complete() {
+    callbackCount++;
+    if (callbackCount >= 1) {
+      res.render('doctor', context);
     }
 
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
+  }
+});
+/* Adds a doctor*/
 
-    router.get('/', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        var mysql = req.app.get('mysql');
-        getDoctors(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 1){
-                res.render('doctor', context);
-            }
-
-        }
-    });
-
-
-    /* Adds a person, redirects to the people page after adding */
-
-    router.post('/', function(req, res){
-        console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO doctor (ID,first_name, last_name, C_ID) VALUES (?,?,?,?)";
-        var inserts = [req.body.ID, req.body.first_name, req.body.last_name, req.body.C_ID];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/doctors');
-            }
-        });
-    });
-
+router.post('/', function(req, res) {
+  console.log(req.body)
+  var pharmacy = req.app.get('pharmacy');
+  var sql = "INSERT INTO doctor (ID,first_name, last_name, C_ID) VALUES (?,?,?,?)";
+  var inserts = [req.body.ID, req.body.first_name, req.body.last_name, req.body.C_ID];
+  sql = pharmacy.pool.query(sql, inserts, function(error, results, fields) {
+    if (error) {
+      console.log(JSON.stringify(error))
+      res.write(JSON.stringify(error));
+      res.end();
+    } else {
+      res.redirect('/doctors');
+    }
+  });
+});
 
