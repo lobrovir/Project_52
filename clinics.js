56e21f44cca80ea67ffec0b456ea144cff29957e
Ryan Lobro <56172426+lobrovir@users.noreply.github.com>
2019-11-18 15:48:09 -0800

    Update clinics.js

--- clinics.js
+++ clinics.js
@@ -1,51 +1,45 @@
-module.exports = function(){
-    var express = require('express');
-    var router = express.Router();
+module.exports = function() {
+  var express = require('express');
+  var router = express.Router();
 
-    function getClinics(res, mysql, context, complete){
-        mysql.pool.query("SELECT ID, name, address, city, state, zip FROM clinic, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.people = results;
-            complete();
-        });
+  function getClinics(res, pharmacy, context, complete) {
+    pharmacy.pool.query("SELECT ID, name, address, city, state, zip FROM clinic, function(error, results, fields){
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.people = results; complete();
+    });
+}
+
+router.get('/', function(req, res) {
+  var callbackCount = 0;
+  var context = {};
+  var pharmacy = req.app.get('pharmacy');
+  getClinics(res, pharmacy, context, complete);
+
+  function complete() {
+    callbackCount++;
+    if (callbackCount >= 1) {
+      res.render('clinic', context);
     }
 
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
+  }
+});
 
-    router.get('/', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        var mysql = req.app.get('mysql');
-        getClinics(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 1){
-                res.render('clinic', context);
-            }
-
-        }
-    });
-
-
-    /* Adds a person, redirects to the people page after adding */
-
-    router.post('/', function(req, res){
-        console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO clinic (ID, name, address, city, state, zip) VALUES (?,?,?,?,?,?)";
-        var inserts = [req.body.ID, req.body.name, req.body.address,req.body.city,req.body.state,req.body.zip];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/clinics');
-            }
-        });
-    });
-
-
+/* Adds a clinic */
+router.post('/', function(req, res) {
+  console.log(req.body)
+  var pharmacy = req.app.get('pharmacy');
+  var sql = "INSERT INTO clinic (ID, name, address, city, state, zip) VALUES (?,?,?,?,?,?)";
+  var inserts = [req.body.ID, req.body.name, req.body.address, req.body.city, req.body.state, req.body.zip];
+  sql = pharmacy.pool.query(sql, inserts, function(error, results, fields) {
+    if (error) {
+      console.log(JSON.stringify(error))
+      res.write(JSON.stringify(error));
+      res.end();
+    } else {
+      res.redirect('/clinics');
+    }
+  });
+});
