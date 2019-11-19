87f6852e88c0dcb4e684d3e1b362bc6b86b96a5d
Ryan Lobro <56172426+lobrovir@users.noreply.github.com>
2019-11-18 16:02:48 -0800

    Update patients.js

--- patients.js
+++ patients.js
@@ -1,51 +1,45 @@
-module.exports = function(){
-    var express = require('express');
-    var router = express.Router();
+module.exports = function() {
+  var express = require('express');
+  var router = express.Router();
 
-    function getPatients(res, mysql, context, complete){
-        mysql.pool.query("SELECT patient.SSN as id, first_name, last_name, birthdate FROM patient, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.people = results;
-            complete();
-        });
+  function getPatients(res, pharmacy, context, complete) {
+    pharmacy.pool.query("SELECT SSN, first_name, last_name, birthdate FROM patient, function(error, results, fields){
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.patients = results; /*May need to change to patient, same possible issue with other pages*/ complete();
+    });
+}
+
+router.get('/', function(req, res) {
+  var callbackCount = 0;
+  var context = {};
+  var pharmacy = req.app.get('pharmacy');
+  getPatients(res, pharmacy, context, complete);
+
+  function complete() {
+    callbackCount++;
+    if (callbackCount >= 1) {
+      res.render('patients', context);
     }
 
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
+  }
+});
+/* Adds a patient*/
 
-    router.get('/', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        var mysql = req.app.get('mysql');
-        getPatients(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 1){
-                res.render('people', context);
-            }
-
-        }
-    });
-
-
-    /* Adds a person, redirects to the people page after adding */
-
-    router.post('/', function(req, res){
-        console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO patient (SSN, first_name, last_name, birthdate) VALUES (?,?,?,?)";
-        var inserts = [req.body.SSN, req.body.first_name, req.body.last_name, req.body.birthdate];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/patients');
-            }
-        });
-    });
-
-
+router.post('/', function(req, res) {
+  console.log(req.body)
+  var pharmacy = req.app.get('pharmacy');
+  var sql = "INSERT INTO patient (SSN, first_name, last_name, birthdate) VALUES (?,?,?,?)";
+  var inserts = [req.body.SSN, req.body.first_name, req.body.last_name, req.body.birthdate];
+  sql = pharmacy.pool.query(sql, inserts, function(error, results, fields) {
+    if (error) {
+      console.log(JSON.stringify(error))
+      res.write(JSON.stringify(error));
+      res.end();
+    } else {
+      res.redirect('/patients');
+    }
+  });
+});
