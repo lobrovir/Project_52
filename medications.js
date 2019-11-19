377e9f4a226dd7c4e4a0d45bc3c3d798a282cff1
Ryan Lobro <56172426+lobrovir@users.noreply.github.com>
2019-11-18 18:12:06 -0800

    Update medications.js

--- medications.js
+++ medications.js
@@ -2,8 +2,8 @@ module.exports = function() {
   var express = require('express');
   var router = express.Router();
 
-  function getMedications(res, pharmacy, context, complete) {
-    pharmacy.pool.query("SELECT ID, name, p_safe FROM medication, function(error, results, fields){
+  function getMedications(res, mysql, context, complete) {
+    mysql.pool.query("SELECT ID, name, p_safe FROM medication, function(error, results, fields){
       if (error) {
         res.write(JSON.stringify(error));
         res.end();
@@ -15,8 +15,8 @@ module.exports = function() {
 router.get('/', function(req, res) {
   var callbackCount = 0;
   var context = {};
-  var pharmacy = req.app.get('pharmacy');
-  getMedications(res, pharmacy, context, complete);
+  var mysql = req.app.get('mysql');
+  getMedications(res, mysql, context, complete);
 
   function complete() {
     callbackCount++;
@@ -29,10 +29,10 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
   console.log(req.body)
-  var pharmacy = req.app.get('pharmacy');
+  var mysql = req.app.get('mysql');
   var sql = "INSERT INTO medication (ID, name, p_safe) VALUES (?,?,?)";
   var inserts = [req.body.ID, req.body.name, req.body.p_safe];
-  sql = pharmacy.pool.query(sql, inserts, function(error, results, fields) {
+  sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
     if (error) {
       console.log(JSON.stringify(error))
       res.write(JSON.stringify(error));
